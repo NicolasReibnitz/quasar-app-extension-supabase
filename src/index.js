190b39b61966ac05378e439be66baf6e1368ea98
Luke Diebold <luke@ldiebold.com>
2022-01-25 09:53:33 +1000

    v0.0.5

--- src/index.js
+++ src/index.js
@@ -4,15 +4,6 @@ module.exports = function (api) {
   api.compatibleWith('quasar', '^2.0.0')
   api.compatibleWith('@quasar/app', '^3.0.0')
 
-  // There may already be a config file for supabase
-  // if @vueauth/supabase has created it already.
-  // We check if it exists before rendering
-  const dir = api.resolve.src()
-  const configFile = dir + '/config/supabase.js'
-  if(!fs.existsSync(configFile)) {
-    api.render('./templates')
-  }
-
   // ensure boot file is registered
   api.extendQuasarConf((conf, api) => {
     conf.boot.push('~quasar-app-extension-my-ext/src/boot/vue-model-supabase-install.js')
--- src/install.js
+++ src/install.js
@@ -1,6 +1,15 @@
 var fs = require('fs')
 
 module.exports = function (api) {
+  // There may already be a config file for supabase
+  // if @vueauth/supabase has created it already.
+  // We check if it exists before rendering
+  const dir = api.resolve.src('config')
+  const configFile = dir + '/config/supabase.js'
+  if(!fs.existsSync(configFile)) {
+    api.render('./templates')
+  }
+
   // ensure @vuemodel/supabase is installed
   api.extendPackageJson({
     dependencies: {
