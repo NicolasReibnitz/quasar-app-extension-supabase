da4de296997502d563235f5528b659082eec203d
Nicolas Reibnitz <nicolas.reibnitz@simpleshow.com>
2022-03-26 23:26:53 -0400

    Fix: Vite unable to import @vuemodel/supabase

--- src/boot/vue-model-supabase-install.js
+++ src/boot/vue-model-supabase-install.js
@@ -1,4 +1,4 @@
-import { SupabasePlugin } from '@vuemodel/supabase'
+import { SupabasePlugin } from '@vuemodel/supabase/src/main'
 import supabaseConfig from 'app/config/supabase.js'
 
 export default ({ app }) => {
